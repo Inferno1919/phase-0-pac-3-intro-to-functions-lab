9df96d63a371038926746d364a41d8a58930f58b
Jake Speitel <jakespeitel19@gmail.com>
2022-04-12 19:40:47 -0400

    commit tree 14373c6f6524cdd461fb262797e6446f21a1b088

---
+++ index.js
@@ -0,0 +1,41 @@
+
+function shout(string){
+    return string.toUpperCase();
+
+}
+
+function whisper(string){
+    return string.toLowerCase();
+
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+    
+}
+function sayHiToHeadphonedRoommate(string){
+    if(console.log(string === string.toLowerCase( ))){
+      
+        sayHiToHeadphonedRoommate = 'I can\'t hear you!'
+    }
+    else if(console.log(string === string.toUpperCase( ))){
+
+        sayHiToHeadphonedRoommate = 'YES INDEED!'
+    }
+    else if(console.log(string === 'Let\'s have dinner together!')){
+
+        sayHiToHeadphonedRoommate = 'I would love to!'
+
+    }
+
+
+
+    return sayHiToHeadphonedRoommate(string);
+
+}
+
